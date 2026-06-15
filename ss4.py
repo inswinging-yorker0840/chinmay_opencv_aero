e75a04432d060d294f287930fac8781a27de6157
Chinmay Rakesh <chinmayrakesh@gmail.com>
2026-06-15 23:21:15 +0400

    done

--- ss4.py
+++ ss4.py
@@ -47,16 +47,59 @@ plt.subplot(131); plt.imshow(img_rgb_darker);  plt.title("Darker");
 plt.subplot(132); plt.imshow(img_rgb);         plt.title("Original");
 plt.subplot(133); plt.imshow(img_rgb_brighter);plt.title("Brighter");'''
 
-matrix1 = np.ones(img_rgb.shape) * 0.8
+'''matrix1 = np.ones(img_rgb.shape) * 0.8
 matrix2 = np.ones(img_rgb.shape) * 1.2
 
 img_rgb_darker   = np.uint8(cv2.multiply(np.float64(img_rgb), matrix1))#convert to float64 to avoid overflow
 #multiply the image with a matrix of 0.8 to reduce the contrast
 #convert back to uint8 to display the image
-img_rgb_brighter = np.uint8(cv2.multiply(np.float64(img_rgb), matrix2))
+img_rgb_brighter = np.uint8(np.clip(cv2.multiply(np.float64(img_rgb), matrix2), 0, 255))#stop changing colour due to overflow by clipping the values between 0 and 255
 
 # Show the images
 plt.figure(figsize=[18,5])
 plt.subplot(131); plt.imshow(img_rgb_darker);  plt.title("Lower Contrast");
 plt.subplot(132); plt.imshow(img_rgb);         plt.title("Original");
 plt.subplot(133); plt.imshow(img_rgb_brighter);plt.title("Higher Contrast");
+plt.show()'''
+
+# Read the original image
+'''img_read = cv2.imread("Piano_Sheet_Music.png", cv2.IMREAD_GRAYSCALE)
+
+# Perform global thresholding
+retval, img_thresh_gbl_1 = cv2.threshold(img_read, 50, 255, cv2.THRESH_BINARY)
+
+# Perform global thresholding
+retval, img_thresh_gbl_2 = cv2.threshold(img_read, 130, 255, cv2.THRESH_BINARY)
+
+# Perform adaptive thresholding
+img_thresh_adp = cv2.adaptiveThreshold(img_read, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,7 )
+img_thresh_adp1 = cv2.adaptiveThreshold(img_read, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 17,9)'''
+
+
+# Show the images
+'''plt.figure(figsize=[18,15])
+plt.subplot(231); plt.imshow(img_read,        cmap="gray");  plt.title("Original");
+plt.subplot(232); plt.imshow(img_thresh_gbl_1,cmap="gray");  plt.title("Thresholded (global: 50)");
+plt.subplot(233); plt.imshow(img_thresh_gbl_2,cmap="gray");  plt.title("Thresholded (global: 130)");
+plt.subplot(234); plt.imshow(img_thresh_adp,  cmap="gray");  plt.title("Thresholded (adaptive)");
+plt.subplot(235); plt.imshow(img_thresh_adp1, cmap="gray");  plt.title("Thresholded (adaptive, mean)");
+plt.show()'''
+
+
+apple = cv2.imread(
+    r"C:\Users\User\Downloads\apple.jpg",
+    cv2.IMREAD_GRAYSCALE
+)
+ball=cv2.imread(
+    r"C:\Users\User\Downloads\ball.jpg",
+    cv2.IMREAD_COLOR
+)
+print(apple.shape)
+print(ball.shape)
+
+width=ball.shape[1]
+height=ball.shape[0]
+apple_resized=cv2.resize(apple,(width,height),interpolation=cv2.INTER_LINEAR)
+result = cv2.bitwise_and(ball, ball, mask=apple_resized)
+plt.imshow(result[:,:,::-1])
+plt.show()
